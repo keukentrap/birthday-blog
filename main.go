3ecba3751e734e6479ff719f252a3c256f73930c
Wietze Mulder <wmulder@panda.lan>
2019-08-09 15:35:45 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"html/template"
+	"net/http"
+)
+
+type user struct {
+	UserName string
+	Password []byte
+	Role string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{} // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+
+	bs, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	dbUsers["kwallie"] = user{"kwallie", bs, "admin"}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/admin", admin)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	u, _ := getUserifLoggedin(req)
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func admin(w http.ResponseWriter, req *http.Request) {
+	u, ok := getUserifLoggedin(req)
+
+	if !ok {
+		http.Redirect(w, req, "/login", http.StatusFound)
+	}
+
+	if u.Role != "admin" {
+		http.Error(w, "Access denied", http.StatusForbidden)
+		return
+	}
+	tpl.ExecuteTemplate(w, "admin.gohtml", u)
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+
+		// create session
+		sID := uuid.New()
+		c := &http.Cookie{
+			Name: "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	// get cookie
+
+	// if the user exists already, get user
+
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		pw := req.FormValue("password")
+
+		// username taken?
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already taken", http.StatusForbidden)
+			return
+		}
+
+		// create session
+		sID := uuid.New()
+		c := &http.Cookie{
+			Name: "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+
+		// store user in dbUsers
+		bs, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		r := "nobody"
+		u := user{un, bs, r}
+		dbUsers[un] = u
+
+		// redirect
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+}
+
+func logout(w http.ResponseWriter, req *http.Request) {
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+	}
+	c, _ := req.Cookie("session")
+	delete(dbSessions, c.Value)
+
+	c = &http.Cookie{
+		Name: "session",
+		Value: "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, c)
+
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func getUserifLoggedin(req *http.Request) (user,bool) {
+	c, err := req.Cookie("session")
+	if err != nil {
+		return user{},false
+	}
+	un := dbSessions[c.Value]
+	u, ok := dbUsers[un]
+	return u, ok
+}
+
+
+func alreadyLoggedIn(req *http.Request) bool {
+	c, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+	un := dbSessions[c.Value]
+	_, ok := dbUsers[un]
+	return ok
+}
